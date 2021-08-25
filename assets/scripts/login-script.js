51abce0f30e4e39a35fed6baf357c55f3e013c75
Vicx99 <89523435+Vicx99@users.noreply.github.com>
2021-08-25 21:46:12 +0700

    Update login-script.js
    Penghilangan tanda pagar

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -10,19 +10,19 @@
 * Membuat variabel LoginForElement untuk tampilan form.
 * @constant {HTMLElement}
 */
-const loginFormElement = document.querySelector('#loginForm');
+const loginFormElement = document.querySelector('loginForm');
 
 /**
 * Membuat variabel inputEmailElement untuk tampilan input email.
 * @constant {HTMLElement}
 */
-const inputEmailElement = document.querySelector('#inputEmail');
+const inputEmailElement = document.querySelector('inputEmail');
 
 /**
 * Membuat variabel inputPasswordElement untuk tampilan input password.
 * @constant {HTMLElement}
 */
-const inputPasswordElement = document.querySelector('#inputPassword');
+const inputPasswordElement = document.querySelector('inputPassword');
 
 /**
 *Membuat variabel expectedEmail untuk menyimpan informasi email sementara.
@@ -36,6 +36,7 @@ const expectedEmail = 'admin@dicoding.com';
 */
 const expectedPassword = 'superpassword';
 
+/*Menambahkan aksi klik pada button */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
   
